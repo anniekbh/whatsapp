b99cebc35254dda804979c99a3e6a6c2bac577cd
anniekbh <anniekbh@gmail.com>
2016-10-10 20:16:32 -0500

    agregando eventos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 window.addEventListener("load", function(){
 	var casilla = document.getElementById("mensajes");
-	var contenedor = document.getElementById("chat");
+	//var contenedor = document.getElementById("chat");
+	var conversacion = document.getElementById("conversacion");
 
-	casilla.addEventListener("keydown", function(){
-		if("keypress" !== "enter");
-		imprimirMensaje(casilla.value,nuevaBurbuja.value);
+	casilla.addEventListener("keyup", function(event){
+		if(event.keyCode == 13){
+			imprimirMensaje();
+			casilla.value="";
+		}
 	});	
-});
+		
+	
+	function imprimirMensaje(){
+		var caja = document.createElement("div");
+		var cajita = document.createElement("div");
+		var texto = document.createElement("p");
+		var clock = document.createElement("div");
 
-function imprimirMensaje(casilla,contenedor,nuevaBurbuja){
-	this.casilla = document.createElement("div");
-	this.nuevaBurbuja = document.createTextNode(casilla);
-	casilla.appendChild(nuevaBurbuja);
-	contenedor.inserBefore(casilla,contenedor.childNodes[0]);
-};
+		caja.classList.add("w-message", "w-message-out");
+		conversacion.appendChild(caja);
+		
+		cajita.classList.add("w-message-text");
+		caja.insertBefore(cajita, caja.children[0]);
+		
+		texto.textContent = casilla.value;
+		cajita.insertBefore(texto, cajita.children[0]);
+
+		clock.classList.add("time");
+		clock.textContent = new Date().toLocaleTimeString(navigator.language, {hour:"2-digit", minute:"2-digit"});
+		cajita.appendChild(clock);
+	};
+});
